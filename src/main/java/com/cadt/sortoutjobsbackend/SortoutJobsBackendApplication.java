e9d22de5e3043422b189d0e25fbb515001e2d44d
ajaySingh2615 <ajaysingh261526@gmail.com>
2026-02-14 19:01:06 +0530

    Initial commit of the project structure and setup.

---
+++ src/main/java/com/cadt/sortoutjobsbackend/SortoutJobsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cadt.sortoutjobsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortoutJobsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortoutJobsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cadt/sortoutjobsbackend/SortoutJobsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadt.sortoutjobsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortoutJobsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
